b12b5aecf19d5760394f3461c1ca5d3b3a623a4c
Brian Karfunkel <brian.karfunkel@nyu.edu>
2015-06-24 14:35:10 -0400

    Add get_links function

--- dl_acs.py
+++ dl_acs.py
@@ -1,6 +1,34 @@
 import os
+import requests
+from bs4 import BeautifulSoup as bs
 import click
 
+
+def get_links(url, filter=None):
+    """Return (filtered) links listed in an HTML table at a given URL.
+
+    Args:
+    	url: URL where listing is located
+		link_filter (Optional[Callable[str, obj]]): Function to filter links.
+			Should take the link as an argument and return ``None`` if link should
+			not be added to list and an object (usually a dict or str) if it should.
+			The object returned will be added to the list of links as is.
+
+	Returns:
+		List: A list of (possibly filtered) links. If no filter is provided, simply
+			a list of the links themselves, as strings. If a filter is provided,
+			a list of the output from the filter.
+
+	"""
+    r = requests.get(url)
+    soup = bs(r.text)
+    links = soup.select('td a') # get all links in a table cell
+
+    if filter is not None:
+    	return [filter(link['href']) for link in links]
+    else:
+    	return [link['href'] for link in links]
+    
 
 class CensusDownloader(object):
 	"""Class to hold downloader context.
@@ -29,6 +57,16 @@ class CensusDownloader(object):
 			if not os.path.isdir(outdir):
 				raise
 		self._outdir = outdir
+
+	@property
+	def remote_files(self):
+	    return self._remote_files
+
+	@property
+	def dest_files(self):
+	    return self._dest_files
+	
+	
 
 @click.group()
 def dl_acs():
